48d32a8d5c5abd536ba284e59ad9d66b430c0871
vitornogueira <vitor_mbn@hotmail.com>
2022-04-13 21:35:51 -0300

    Criacao do projeto react router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Bapnovo from "./routes/bapnovo";
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes,
+  Route, } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+      <Route path="bapnovo" element={<Bapnovo />} />
+    </Routes>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/routes/bapnovo.jsx
@@ -0,0 +1,7 @@
+export default function Bapnovo() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Bapnovo</h2>
+      </main>
+    );
+  }
